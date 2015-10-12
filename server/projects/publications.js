514a59bcf97efaa643d0537c953416c5df379d4e
Lord987 <204172@supinfo.com>
2015-10-12 12:09:39 +0200

    Show projects

--- server/projects/publications.js
+++ server/projects/publications.js
@@ -14,3 +14,20 @@ Meteor.publishComposite('projects-portfolio', function () {
         ]
     }
 });
+
+Meteor.publishComposite('projects-show', function(id) {
+    return {
+        find: function () {
+            var view = Projects.views.show(id);
+            return Projects.find(view.selector, view.options);
+        },
+        children: [
+            {
+                find: function (project) {
+                    var view = Technologies.views.list(project.technologies);
+                    return Technologies.find(view.selector, view.options);
+                }
+            }
+        ]
+    }
+});
